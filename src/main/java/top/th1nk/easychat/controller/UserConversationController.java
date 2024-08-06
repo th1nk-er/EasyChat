6cd45ba206193f7ce4df674db208e832cef30e85
th1nk-er <vinkangnet@gmail.com>
2024-08-06 15:55:56 +0800

    refactor(controller): rename ChatHistoryController to UserConversationController
    Rename the ChatHistoryController class to UserConversationController to better reflect its
    functionality. Additionally, implement new methods to handle conversation list retrieval.

---
+++ src/main/java/top/th1nk/easychat/controller/UserConversationController.java
@@ -0,0 +1,28 @@
+package top.th1nk.easychat.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.th1nk.easychat.domain.Response;
+import top.th1nk.easychat.domain.SysUserConversation;
+import top.th1nk.easychat.service.SysUserConversationService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/conversation")
+@Tag(name = "用户对话模块", description = "用户对话API")
+public class UserConversationController {
+    @Resource
+    private SysUserConversationService sysUserConversationService;
+
+    @Operation(summary = "获取用户对话列表", description = "获取用户对话列表")
+    @GetMapping("/list/{pageNum}")
+    public Response<List<SysUserConversation>> getConversations(@PathVariable int pageNum) {
+        return Response.ok(sysUserConversationService.getChatHistory(pageNum));
+    }
+}
