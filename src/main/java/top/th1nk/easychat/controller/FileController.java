c84ed8dac53d026253e4ddd840e3ebcbbde59ea9
th1nk-er <vinkangnet@gmail.com>
2024-09-02 18:26:58 +0800

    feat(file): add group avatar support and optimize image retrieval
    Implement a new feature to support group avatars by adding the necessary endpoints
    and logic to handle the retrieval and display of group avatars. Refactor the existing
    image retrieval mechanism to improve code reusability and reduce redundancy. This includes
    optimizing the getAvatar and getChatImg methods by introducing a generalized method
    for image retrieval.

--- src/main/java/top/th1nk/easychat/controller/FileController.java
+++ src/main/java/top/th1nk/easychat/controller/FileController.java
@@ -5,12 +5,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import top.th1nk.easychat.config.easychat.ChatProperties;
+import top.th1nk.easychat.config.easychat.GroupProperties;
 import top.th1nk.easychat.config.easychat.UserProperties;
 import top.th1nk.easychat.domain.Response;
 import top.th1nk.easychat.exception.CommonException;
@@ -33,26 +35,41 @@ public class FileController {
     private UserProperties userProperties;
     @Resource
     private ChatProperties chatProperties;
+    @Resource
+    private GroupProperties groupProperties;
+
+    /**
+     * 从minio中获取图片，并以ResponseEntity类型返回
+     *
+     * @param imgName 图片文件名
+     * @param imgDir  图片文件夹
+     * @return ResponseEntity
+     */
+    @NotNull
+    private ResponseEntity<byte[]> getImageResponseEntity(String imgName, String imgDir) throws IOException {
+        GetObjectResponse objectResponse = minioService.getObject(imgDir + "/" + imgName);
+        if (objectResponse == null) return ResponseEntity.notFound().build();
+        String contentType = objectResponse.headers().get("Content-Type");
+        if (contentType == null) contentType = "image/jpeg";
+        byte[] avatarBytes = objectResponse.readAllBytes();
+        objectResponse.close();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + imgName + "\"")
+                .contentType(MediaType.valueOf(contentType))
+                .body(avatarBytes);
+    }
 
     @Operation(summary = "获取头像", description = "以文件形式返回用户头像")
     @GetMapping("/avatar/{imgName}")
     public ResponseEntity<byte[]> getAvatar(@PathVariable String imgName) {
-        GetObjectResponse objectResponse = minioService.getObject(userProperties.getAvatarDir() + "/" + imgName);
-        if (objectResponse == null) return ResponseEntity.notFound().build();
-        String contentType = objectResponse.headers().get("Content-Type");
-        if (contentType == null) contentType = "image/jpeg";
         try {
-            byte[] avatarBytes = objectResponse.readAllBytes();
-            objectResponse.close();
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + imgName + "\"")
-                    .contentType(MediaType.valueOf(contentType))
-                    .body(avatarBytes);
-        } catch (Exception e) {
+            return getImageResponseEntity(imgName, userProperties.getAvatarDir());
+        } catch (IOException e) {
             log.error("获取头像失败", e);
             return ResponseEntity.internalServerError().build();
         }
     }
+
 
     @Operation(summary = "上传聊天图片", description = "上传聊天图片")
     @PostMapping("/chat/image")
@@ -76,22 +93,24 @@ public class FileController {
         }
     }
 
-    @Operation(summary = "获取图片", description = "以文件形式返回聊天图片")
+    @Operation(summary = "获取聊天图片", description = "以文件形式返回聊天图片")
     @GetMapping("/chat-file/{imgDate}/{imgName}")
     public ResponseEntity<byte[]> getChatImg(@PathVariable String imgDate, @PathVariable String imgName) {
-        GetObjectResponse objectResponse = minioService.getObject(chatProperties.getFileDir() + "/" + imgDate + "/" + imgName);
-        if (objectResponse == null) return ResponseEntity.notFound().build();
-        String contentType = objectResponse.headers().get("Content-Type");
-        if (contentType == null) contentType = "image/jpeg";
         try {
-            byte[] avatarBytes = objectResponse.readAllBytes();
-            objectResponse.close();
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + imgName + "\"")
-                    .contentType(MediaType.valueOf(contentType))
-                    .body(avatarBytes);
-        } catch (Exception e) {
+            return getImageResponseEntity(imgName, chatProperties.getFileDir() + "/" + imgDate);
+        } catch (IOException e) {
             log.error("获取聊天图片失败", e);
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @Operation(summary = "获取群头像", description = "以文件形式返回群头像")
+    @GetMapping("/group/avatar/{imgName}")
+    public ResponseEntity<byte[]> getGroupAvatar(@PathVariable String imgName) {
+        try {
+            return getImageResponseEntity(imgName, groupProperties.getAvatarDir());
+        } catch (IOException e) {
+            log.error("获取群头像失败", e);
             return ResponseEntity.internalServerError().build();
         }
     }
